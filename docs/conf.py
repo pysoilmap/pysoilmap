d57b507c2b4b310adab8a2261ad1578ae13820bd
Thomas Gläßle <t_glaessle@gmx.de>
2021-04-13 12:28:55 +0200

    Remove some cluttering comments in the docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For more information, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 import pysoilmap
 
@@ -21,16 +9,11 @@ import pysoilmap
 project = 'pysoilmap'
 copyright = '2021, Thomas Gläßle'
 author = 'Thomas Gläßle'
-
-# The full version, including alpha/beta/rc tags
 release = pysoilmap.__version__
 
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.viewcode',
     'sphinx_automodapi.automodapi',
@@ -44,15 +27,9 @@ automodsumm_inherited_members = True
 typehints_fully_qualified = False
 typehints_document_rtype = True
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'alabaster'
