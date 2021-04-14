059f4465068e4895ed35ecc12166851429d80397
Thomas Gläßle <t_glaessle@gmx.de>
2021-04-14 18:13:43 +0200

    Rework CLI using chained subcommands [experimental]

--- src/pysoilmap/cli/shapefile.py
+++ src/pysoilmap/cli/shapefile.py
@@ -7,95 +7,98 @@ import pysoilmap.shapeops as shapeops
 import click
 import geopandas as gpd
 
+import functools
 import os
 
 
-@click.group('shapefile')
-def main(args=None):
+def needs_gdf(func):
+    """Marks a callback as wanting to receive the current GeoDataFrame
+    object as first argument."""
+    def wrapper(*args, **kwargs):
+        ctx = click.get_current_context().find_root()
+        gdf = ctx.obj
+        if gdf is None:
+            raise click.ClickException(
+                f"Please use `read <file>` before the '{ctx.command.name}' "
+                "command!")
+        return func(gdf, *args, **kwargs)
+    return functools.update_wrapper(wrapper, func)
+
+
+def returns_gdf(func):
+    """Marks a callback as returning the new GeoDataFrame object."""
+    def wrapper(*args, **kwargs):
+        ctx = click.get_current_context().find_root()
+        ctx.obj = func(*args, **kwargs)
+        return ctx.obj
+    return functools.update_wrapper(wrapper, func)
+
+
+@click.group(
+    'shapefile',
+    chain=True,
+    no_args_is_help=True,
+    subcommand_metavar='(<command> [<args>...])...',
+)
+def main():
     """Execute shapefile command."""
     pass
 
 
 @main.command()
-@click.argument('input_file')
-def info(input_file):
-    """Show info about shapefile and its columns."""
-    gdf = _read_shapefile(input_file)
-    gdf.info(verbose=True, show_counts=True)
-
-
-@main.command()
-@click.option('-o', '--output', 'output_file')
-@click.argument('input_file')
-def export(input_file, output_file):
-    """Export shapefile data columns to .csv or .tsv."""
-    ext = os.path.splitext(output_file)[1].lower()
-    if ext not in ('.txt', '.tsv', '.csv'):
-        raise click.ClickException(
-            "Unknown output file extension: {!r}".format(output_file))
-
-    gdf = _read_shapefile(input_file)
-    if ext in ('.txt', '.tsv'):
-        print("Writing to: {}".format(output_file))
-        gdf.to_csv(output_file, sep='\t')
-    elif ext == '.csv':
-        print("Writing to: {}".format(output_file))
-        gdf.to_csv(output_file)
-
-
-@main.command()
-@click.argument('input_file')
-@click.argument('distance', type=float)
-@click.option('-o', '--output', 'output_file', help='Output file')
-@click.option('--inplace', is_flag=True, help='Use input file as output file')
-def buffer(input_file, output_file, distance: float, inplace):
-    """Apply ``shape.buffer(...)`` to each shape in a shapefile."""
-    print(output_file, inplace)
-    if inplace:
-        if output_file is not None:
-            raise click.UsageError("--inplace is incompatible with --output!")
-        output_file = input_file
-    elif output_file is None:
-        raise click.UsageError(
-            "Missing output file name. You must specify either "
-            "'--output' or '--inplace'!")
-
-    gdf = _read_shapefile(input_file)
-    print("Buffering geometries…")
-    gdf = shapeops.buffer(gdf, distance)
-    _write_shapefile(gdf, output_file)
-
-
-@main.command()
-@click.argument('input_file')
-@click.option('-o', '--output', 'output_file', help='Output file')
-@click.option('--inplace', is_flag=True, help='Use input file as output file')
-def repair(input_file, output_file, inplace):
-    """Attempt to fix broken geometries in the given shapefile."""
-    if inplace:
-        if output_file is not None:
-            raise click.UsageError("--inplace is incompatible with --output!")
-        output_file = input_file
-    elif output_file is None:
-        raise click.UsageError(
-            "Missing output file name. You must specify either "
-            "'--output' or '--inplace'!")
-    gdf = _read_shapefile(input_file)
-    print("Repairing geometries…")
-    gdf = shapeops.repair(gdf)
-    _write_shapefile(gdf, output_file)
-
-
-def _read_shapefile(filename):
-    """Read shapefile."""
-    print("Reading shapefile: {}".format(filename))
+@click.argument('file', metavar='<file>')
+@returns_gdf
+def read(file):
+    """Read shapefile - always lead with this command!"""
+    print("Reading shapefile: {}".format(file))
     try:
-        return gpd.read_file(filename)
+        return gpd.read_file(file)
     except Exception as e:
         raise click.ClickException(e)
 
 
-def _write_shapefile(gdf, filename):
-    """Write shapefile."""
-    print("Writing to: {}".format(filename))
-    gdf.to_file(filename)
+@main.command()
+@click.argument('file', metavar='<file>')
+@needs_gdf
+def write(gdf, file):
+    """
+    Write shapefile. You should usually close with this command.
+
+    Note that in addition to '.shp' can can also write to '.csv', '.tsv', or
+    '.txt'. In this case, only the row data without shape information will be
+    exported.
+    """
+    print("Writing to: {}".format(file))
+    ext = os.path.splitext(file)[1].lower()
+    if ext in ('.txt', '.tsv'):
+        gdf.to_csv(file, index=False, sep='\t')
+    elif ext == '.csv':
+        gdf.to_csv(file, index=False)
+    else:
+        gdf.to_file(file)
+
+
+@main.command()
+@needs_gdf
+def info(gdf):
+    """Show info about shapefile and its columns."""
+    gdf.info(verbose=True, show_counts=True)
+
+
+@main.command()
+@click.argument('distance', metavar='<distance>', type=float)
+@returns_gdf
+@needs_gdf
+def buffer(gdf, distance: float):
+    """Apply ``shape.buffer(...)`` to each shape in a shapefile."""
+    print("Buffering geometries…")
+    return shapeops.buffer(gdf, distance)
+
+
+@main.command()
+@returns_gdf
+@needs_gdf
+def repair(gdf):
+    """Attempt to fix broken geometries in the given shapefile."""
+    print("Repairing geometries…")
+    return shapeops.repair(gdf)
